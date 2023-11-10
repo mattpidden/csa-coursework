2c31007f90d9c3381f93d81b7e6876d0137fdd15
Matthew Pidden <bb22475@bristol.ac.uk>
2023-11-10 21:15:20 +0000

    added IO channels

--- DistributedImplementation/gol/gol.go
+++ DistributedImplementation/gol/gol.go
@@ -14,14 +14,17 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 	//	TODO: Put the missing channels in here.
 
 	ioCommand := make(chan ioCommand)
+	filename := make(chan string)
+	output := make(chan uint8)
+	input := make(chan uint8)
 	ioIdle := make(chan bool)
 
 	ioChannels := ioChannels{
 		command:  ioCommand,
 		idle:     ioIdle,
-		filename: nil,
-		output:   nil,
-		input:    nil,
+		filename: filename,
+		output:   output,
+		input:    input,
 	}
 	go startIo(p, ioChannels)
 
@@ -29,9 +32,9 @@ func Run(p Params, events chan<- Event, keyPresses <-chan rune) {
 		events:     events,
 		ioCommand:  ioCommand,
 		ioIdle:     ioIdle,
-		ioFilename: nil,
-		ioOutput:   nil,
-		ioInput:    nil,
+		ioFilename: filename,
+		ioOutput:   output,
+		ioInput:    input,
 	}
 	distributor(p, distributorChannels)
 }
