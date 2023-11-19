a4c460b9d7c58f2ea5d4cbfbcc737d6ee6a23aa9
Matthew Pidden <bb22475@bristol.ac.uk>
2023-11-19 22:00:01 +0000

    fixed bug introduced in keypress feature

--- DistributedImplementation/gol_engine/gol_engine.go
+++ DistributedImplementation/gol_engine/gol_engine.go
@@ -169,6 +169,8 @@ func (g *GoLOperations) SingleThreadExecution(req SingleThreadExecutionRequest, 
 		g.totalTurns = req.Turns
 		g.imageWidth = req.ImageWidth
 		g.imageHeight = req.ImageHeight
+		g.turn = 0
+		g.updateGolWorld(req.GolWorld)
 	}
 
 	g.state = Running
@@ -197,7 +199,7 @@ func (g *GoLOperations) SingleThreadExecution(req SingleThreadExecutionRequest, 
 	}
 
 	res.Turns = g.turn
-	res.GolWorld = newGolWorld
+	res.GolWorld = g.getGolWorld()
 	fmt.Println("Finished Running SingleThreadExecution ")
 
 	//If killing selected, let main function know to end it all
